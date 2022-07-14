e5cb20230760f319f8edcc0048b57dbfc5f8a5d3
Sharpryan20 <sharpryan01@icloud.com>
2022-07-14 19:55:53 +0000

    Added SKU field to class Product

--- products/models.py
+++ products/models.py
@@ -16,6 +16,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
