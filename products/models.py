8ed631683ee7f6afbc48ad071c55bbc086579ec4
Sharpryan20 <sharpryan01@icloud.com>
2022-07-18 12:04:42 +0000

    Added image_url field to products models and added image_url field to all products

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class Category(models.Model):
 
@@ -22,6 +23,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    image_url = CloudinaryField('image', default='placeholder')
     
 
     def __str__(self):
