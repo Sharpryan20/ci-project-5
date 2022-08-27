22f3c1b43482a952d417371702613d1129386aee
Sharpryan20 <sharpryan01@icloud.com>
2022-08-27 20:56:45 +0000

    Added blog views, urls and templates

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.views import generic
+from .models import Post
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog/blog.html'
+    paginate_by = 6
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
